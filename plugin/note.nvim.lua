08812a7ed4b7db44c17f24dc0fa7bfb88992acbc
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2025-02-03 22:22:09 +0800

    better ui

--- plugin/note.nvim.lua
+++ plugin/note.nvim.lua
@@ -1,2 +1,2 @@
--- TODO: add portable options: 1. ui options 2. image.nvim dependencies
+-- TODO: add portable options: 1. ui options 2. image.nvim dependencies 3. todo-comment dependencies etc
 require("note").setup()
