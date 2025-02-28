9b6524e7f0cd00da3061e624c5e1de742cb4af43
pxwg <xiangxy23@tsinghua.edu.cn>
2025-02-28 19:44:15 +0800

    rename workflow as phonograph

--- lua/note/keymaps.lua
+++ lua/note/keymaps.lua
@@ -12,7 +12,7 @@ local tags = require("utils.tags")
 --- adding reading states
 --- Mind model: reading paper -> want to take notes about paper -> want to remember the reading state and reload it while review the note -> use this keymapping
 --- workflow: read -> remember (included saving the reading state and get the figure of current path)
-map("n", "<leader>np", function()
+map("n", "<leader>pp", function()
   local tab = rem.InsertPDFurl:pdf()
   if not tab then
     print("Error: rem.InsertPDFurl() returned nil")
@@ -26,12 +26,12 @@ map("n", "<leader>np", function()
   vim.schedule(function()
     prev_pdf.GetFigPath(pdf.path, pdf.page)
   end)
-end, { noremap = true, silent = true, desc = "[N]ew [P]df" })
+end, { noremap = true, silent = true, desc = "[P]hono [P]df" })
 
 --- adding reading states
 --- Mind model: reading paper -> want to take notes about paper -> want to remember the reading state and reload it while review the note -> use this keymapping
 --- workflow: read -> remember (included saving the reading state and get the figure of current path)
-map("n", "<leader>nu", function()
+map("n", "<leader>pu", function()
   local tab = rem.InsertPDFurl:url()
   if not tab then
     print("Error: rem.InsertPDFurl() returned nil")
@@ -41,11 +41,11 @@ map("n", "<leader>nu", function()
   local url = rem.url_line_to_table(tab)
   --- TODO: fully costumizable bookmark
   mark.insert_note_at_cursor({ url.title, url.tag, url.url }, "url")
-end, { noremap = true, silent = true, desc = "[N]ew [U]rl" })
+end, { noremap = true, silent = true, desc = "[P]hono [U]rl" })
 
 --- open pdf reading selection ui
 --- workflow: read -> back to the point of past -> restore the reading state
-map("n", "<leader>nr", function()
+map("n", "<leader>pg", function()
   local path = rem.get_file_path()
 
   local table1 = rem.get_all_pdfs(path)
@@ -60,11 +60,11 @@ map("n", "<leader>nr", function()
   table2 = sel.SortTablebyDistance(indTitle, table2, pos[1])
 
   ui.create_selection_window(table1, table2)
-end, { noremap = true, silent = true, desc = "[N]ote [R]estore" })
+end, { noremap = true, silent = true, desc = "[P]hono [G]raph" })
 
 --- open pdf under cursor
 --- workflow: read -> want to back to the point of past -> restore the reading state
-map("n", "<leader>no", function()
+map("n", "<leader>po", function()
   local current_line = vim.api.nvim_win_get_cursor(0)[1]
   local tag = tags.get_tag_on_line(current_line)
   local path = rem.get_file_path()
@@ -82,4 +82,4 @@ map("n", "<leader>no", function()
     local url = rem.url_line_to_table(line.line)
     api.OpenUntilReady(url.url, url.ScrollY)
   end
-end, { noremap = true, silent = true, desc = "[N]ote [O]pen" })
+end, { noremap = true, silent = true, desc = "[P]hono [O]pen" })
