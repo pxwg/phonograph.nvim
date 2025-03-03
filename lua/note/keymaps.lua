b470b5f2d3c92dd08b9f2f5c05ef12015fe5bd4c
pxwg <xiangxy23@tsinghua.edu.cn>
2025-03-03 11:25:47 +0800

    rewrite the reading state management with sqlite.lua (mostly finished)

--- lua/note/keymaps.lua
+++ lua/note/keymaps.lua
@@ -45,24 +45,6 @@ map("n", "<leader>pu", function()
   mark.insert_note_at_cursor({ url.title, url.tag, url.url }, "url")
 end, { noremap = true, silent = true, desc = "[P]hono [U]rl" })
 
---- open pdf reading selection ui
---- workflow: read -> back to the point of past -> restore the reading state
-map("n", "<leader>pg", function()
-  local path = rem.get_file_path()
-
-  local table1 = rem.get_all_pdfs(path)
-  local table2 = rem.get_all_titles(path)
-
-  local pos = vim.api.nvim_win_get_cursor(0)
-
-  local indPDF = sel.GenerateIndex(table1)
-  local indTitle = sel.GenerateIndex(table2)
-
-  table1 = sel.SortTablebyDistance(indPDF, table1, pos[1])
-  table2 = sel.SortTablebyDistance(indTitle, table2, pos[1])
-
-  ui.create_selection_window(table1, table2)
-end, { noremap = true, silent = true, desc = "[P]hono [G]raph" })
 
 --- open pdf under cursor
 --- workflow: read -> want to back to the point of past -> restore the reading state
