b8a8a81d4f2e64f8dab9ef35f5965a1658d78fa9
pxwg <xiangxy23@tsinghua.edu.cn>
2025-03-03 11:50:46 +0800

    replace workflow with sqlite

--- lua/utils/ui.lua
+++ lua/utils/ui.lua
@@ -30,8 +30,8 @@ local function update_pdf_preview(current_table)
   local fig_path_tab = {}
   for i = 1, #current_table do
     local path = current_table[i].path
-    local page = current_table[i].page
-    fig_path_tab[i] = pdf_preview.TransFigPath(path, page)
+    local pos = current_table[i].pos
+    fig_path_tab[i] = pdf_preview.TransFigPath(path, pos)
   end
   return fig_path_tab
 end
