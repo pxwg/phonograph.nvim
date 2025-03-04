4c528d4a7b385d2667ec00fd8e7b243682932f4e
pxwg <xiangxy23@tsinghua.edu.cn>
2025-03-04 14:10:45 +0800

    delate for some debug prints

--- lua/utils/path.lua
+++ lua/utils/path.lua
@@ -3,7 +3,8 @@ function M.get_db_path()
   local buf = vim.api.nvim_get_current_buf()
   local file_path = vim.api.nvim_buf_get_name(buf)
   file_path = file_path:gsub("^%s+", ""):gsub("%s+$", ""):gsub("%.", ""):gsub("/", "_")
-  return vim.fn.expand("$HOME") .. "/.local/state/nvim/note/" .. file_path .. ".sqlite"
+  local path = vim.fn.expand("$HOME") .. "/.local/state/nvim/note/" .. file_path .. ".sqlite"
+  return path
 end
 
 function M.file_exists(file_path)
